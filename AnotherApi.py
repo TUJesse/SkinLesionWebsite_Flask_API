1e9ada36b26f4071e383867ee91614a3b00bcfab
TUJesse <B00135880@mytudublin.ie>
2024-04-05 02:44:20 +0100

    Initial commit

---
+++ AnotherApi.py
@@ -0,0 +1,96 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from keras.models import load_model
+from keras.preprocessing import image
+from PIL import Image
+import tensorflow as tf
+import numpy as np
+import os
+import joblib
+
+app = Flask(__name__)
+
+SIZE = 224
+SIZE2 = 64
+class_labels = ['Class 0 (akiec)', 'Class 1 (bcc)', 'Class 2 (bkl)', 'Class 3 (df)', 'Class 4 (mel)',
+                    'Class 5 (nv)', 'Class 6 (vasc)']
+
+
+@app.route('/', methods=["POST"])
+def hello_world():
+    dictToReturn = {'text': 'hello world'}
+    return jsonify(dictToReturn)
+
+@app.route('/post', methods=["POST"])
+def testpost():
+     input_json = request.get_json(force=True)
+     dictToReturn = {'text':input_json['text']}
+     return jsonify(dictToReturn)
+
+@app.route('/prediction', methods=["POST"])
+def prediction():
+    #model_path = '"C:\\Users\jesse\OneDrive\Desktop\Year 4\Project\models\Densenetmodel50epochs1500resample224size.keras"'
+    model = load_model(
+        os.path.join('C:\\Users\jesse\OneDrive\Desktop\Year 4\Project\models', 'Densenetmodel50epochs1500resample224size.keras'))
+
+    #file = request.files['file']
+    img = request.files['file']
+    image = Image.open(img)
+    image = tf.image.resize(image, (SIZE, SIZE))
+    image = np.expand_dims(image / 255, axis=0)
+
+    prediction = model.predict(image)
+
+    predicted_label = 'Predicted class is: ' + str(class_labels[prediction.argmax()]) + '. With a probability of ' + str(prediction[0, prediction.argmax()]) + '.'
+
+    dictToReturn = {'': str(predicted_label)}
+
+    return jsonify(dictToReturn)
+
+
+@app.route('/cnnsvm', methods=["POST"])
+def SVM_CNN_prediction():
+    model = load_model(
+        os.path.join('C:\\Users\jesse\OneDrive\Desktop\Year 4\Project\models',
+                     'Densenetmodel50epochs1500resample224size.keras'))
+
+    svm = joblib.load(os.path.join('C:\\Users\jesse\OneDrive\Desktop\Year 4\Project\models',
+                     'svm_model.joblib'))
+
+    img = request.files['file']
+    image = Image.open(img)
+    image = tf.image.resize(image, (SIZE, SIZE))
+    image = np.expand_dims(image / 255, axis=0)
+
+    features = model.predict(image)
+
+    prediction = svm.predict(features)
+
+    predicted_label = class_labels[prediction[0]]
+
+    dictToReturn = {'': str(predicted_label)}
+
+    return jsonify(dictToReturn)
+
+
+@app.route('/pretrained', methods=["POST"])
+def pretrainedCnn():
+    model = load_model(os.path.join('C:\\Users\jesse\OneDrive\Desktop\Year 4\Project\models',
+                     'SequentialRelu50epochs1500resample64size.keras'))
+
+    img = request.files['file']
+    image = Image.open(img)
+    image = tf.image.resize(image, (SIZE2, SIZE2))
+    image = np.expand_dims(image / 255, axis=0)
+
+    prediction = model.predict(image)
+
+    predicted_label = 'Predicted class is: ' + str(class_labels[prediction.argmax()]) + '. With a probability of ' + str(prediction[0, prediction.argmax()]) + '.'
+
+    dictToReturn = {'': str(predicted_label)}
+
+    return jsonify(dictToReturn)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
